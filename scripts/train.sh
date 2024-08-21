e116f266a0318cf2121d573026a948e7c49cfc20
YIN95 <yinw@kth.se>
2024-08-21 10:41:48 +0200

    fix data aug

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 #SBATCH -A berzelius-2024-123
-#SBATCH -t 0-2:0:0
+#SBATCH -t 0-1:0:0
 #SBATCH --gres gpu:1
 #SBATCH --mail-type "BEGIN,END,FAIL"
 #SBATCH --mail-user "yinw@kth.se"
-#SBATCH --output logs/sweep.log
-#SBATCH --error logs/sweep.log
+#SBATCH --output logs/train.log
+#SBATCH --error logs/train.log
 
 
 nvidia-smi
 module load Anaconda/2021.05-nsc1
 conda activate olf
 
+python3 train/percept_regressor.py
 python3 train/mixture_regressor.py
--- train/mixture_regressor.py
+++ train/mixture_regressor.py
@@ -264,3 +264,4 @@ if __name__ == "__main__":
     test_set_submission_output_path = os.path.join(path_output, 'Test_set_Submission_form.csv')
     test_set_submission_df.to_csv(test_set_submission_output_path, index=False)
     print("Done")
+    
