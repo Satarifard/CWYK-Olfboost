b3f35ae49b574434dd6fb3723c15885f2cb35c6a
YIN95 <yinw@kth.se>
2024-08-22 22:54:14 +0200

    ensemble model

---
+++ scripts/batch/1111.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH -A berzelius-2024-123
+#SBATCH -t 0-1:0:0
+#SBATCH --gres gpu:1
+#SBATCH --mail-type "BEGIN,END,FAIL"
+#SBATCH --mail-user "yinw@kth.se"
+#SBATCH --output logs/1111.log
+#SBATCH --error logs/1111.log
+
+nvidia-smi
+module load Anaconda/2021.05-nsc1
+conda activate olf
+
+python3 train/mixture_regressor_ensemble.py --seed 1111
